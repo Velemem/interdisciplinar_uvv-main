64f8bb9a99267378d6722b683048b6310a054502
Arthur Velemem dos Santos <102520517+Velemem@users.noreply.github.com>
2023-11-28 07:05:15 -0300

    Add files via upload

---
+++ pagesComplement/CreatePost/index.js
@@ -0,0 +1,16 @@
+import { Drawer, Header, localStorageManager, pagesEnum} from "../../components/component.js";
+
+localStorageManager(window);
+function appendHtmlString(parentElement, htmlString) {
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = htmlString;
+  
+    while (tempElement.firstChild) {
+      parentElement.appendChild(tempElement.firstChild);
+    }
+};
+
+const topContainer = document.getElementById('top-container');
+appendHtmlString(topContainer, Drawer(pagesEnum[1]));
+appendHtmlString(topContainer, Header());
+
